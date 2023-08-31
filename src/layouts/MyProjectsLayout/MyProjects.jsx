eacb0a6982a792d7609b3995233a9afb5acb0364
Priyal Raj <priyalraj99@gmail.com>
2023-08-31 19:16:10 +0530

    Updated MyProjects Section

--- src/layouts/MyProjectsLayout/MyProjects.jsx
+++ src/layouts/MyProjectsLayout/MyProjects.jsx
@@ -8,26 +8,27 @@ const MyProjects = () => {
 
     const ProjectList = [
         {
-            title: 'karafuru clone',
-            description: 'When I began learning website development, a friend suggested I clone this because it`s quite challenging for a fresher. So, here`s a simple karafuru.io website clone.',
-            link: 'https://capta1nraj.github.io/karafuru-clone/home-page/home-page.html',
-            skills: ['HTML', 'CSS', 'JavaScript'],
-            image: '/assets/images/MyProjectsSectionImages/karafuru.png'
-        },
+            title: 'e-comm admin panel',
+            description: 'A seamless E-Comm admin panel crafted with Next.js. In this project, I implemented custom authentication using bcrypt.js, along with an inbuilt API for effortless user and product management, and more.',
+            link: 'https://ecommerce-admin-dev-branch.vercel.app',
+            skills: ['Next.js', 'Tailwind CSS', 'MongoDB', 'Chart.js', 'Custom Auth'],
+            image: '/assets/images/MyProjectsSectionImages/ecomm-admin-panel.png'
+        }
+        ,
+        {
+            title: 'Connect2Mongodb',
+            description: 'My first ever npm module is a lightweight and easy-to-use library that allows you to connect to MongoDB with both dynamic and fixed databases.',
+            link: 'https://www.npmjs.com/package/connect2mongodb',
+            skills: ['Express.js', 'Mongoose', 'Dotenv'],
+            image: '/assets/images/MyProjectsSectionImages/connect2mongodb.png'
+        }
+        ,
         {
             title: 'loop card',
             description: 'Loop Card is a contact sharing project based on NFC, crafted by me and my team at Alphamint Labs. I took charge of developing the complete reminder feature, which involves a lot of intricate logic.',
             link: 'https://www.loopcard.club',
             skills: ['Next.js', 'Tailwind CSS', 'MongoDB', 'NextAuth.js'],
             image: '/assets/images/MyProjectsSectionImages/loopcard.png'
-        }
-        ,
-        {
-            title: 'e-comm admin panel',
-            description: 'A seamless E-Comm admin panel crafted with Next.js. In this project, I implemented custom authentication using bcrypt.js, along with an inbuilt API for effortless user and product management, and more.',
-            link: 'https://ecommerce-admin-dev-branch.vercel.app',
-            skills: ['Next.js', 'Tailwind CSS', 'MongoDB', 'Chart.js', 'Custom Auth'],
-            image: '/assets/images/MyProjectsSectionImages/ecomm-admin-panel.png'
         }
     ]
 
