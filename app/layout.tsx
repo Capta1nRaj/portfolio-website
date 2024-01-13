042c104a1316e0241a3d80bffa5b0ac5630f352b
Priyal Raj <priyalraj99@gmail.com>
2024-01-14 03:43:23 +0530

    SEO optimized

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Priyal Raj',
   description: 'My name is Priyal Raj, I am a Full Stack Web Dev. Welcome To My Portfolio Website.',
+  keywords: 'full-stack web developer, web development portfolio, priyal raj portfolio, responsive web design, front-end development, back-end development, javascript developer, html5/css3, ui/ux design, web application developer, node.js developer, react.js development, mongodb, express.js, restful apis, mobile-first design, cross-browser compatibility, git version control, portfolio projects, code samples',
+  robots: "index, follow, nocache",
+  category: 'Portfolio',
 }
 
 export default function RootLayout({
