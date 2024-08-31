6e1286be3b7a6ed3edf138e48341e43f6e3d1671
Priyal Raj <priyalraj99@gmail.com>
2024-08-31 19:05:57 +0530

    FetchEachBlogPostViews & BlogPostViewsModel code updated

--- src/app/blog/FetchEachBlogPostViews.ts
+++ src/app/blog/FetchEachBlogPostViews.ts
@@ -5,14 +5,14 @@ import { connect2MongoDB } from "connect2mongodb";
 
 export async function FetchEachBlogPostViews(blogID: string) {
     try {
-
         await connect2MongoDB();
 
-        const postViewsData = await BlogPostViewsModel.findOne({ blogID }).select('-_id views');
-        const postViews = postViewsData.views;
+        let postViewsData = await BlogPostViewsModel.findOne({ blogID }).select('-_id views');
+        if (!postViewsData) { postViewsData = await new BlogPostViewsModel({ blogID, views: 0 }).save(); }
+
+        const { views: postViews } = postViewsData;
 
         return { postViews, status: 200 };
-
     } catch (error) {
 
         console.error("Database operation failed, in FetchEachBlogPostViews if possible, please raise a PR to notify me, thanks ♥.");
--- src/models/BlogPostViewsModel.tsx
+++ src/models/BlogPostViewsModel.tsx
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 
 const BlogPostViewsSchema = new mongoose.Schema({
-    blogID: { type: String },
-    views: { type: Number }
+    blogID: { type: String, required: true },
+    views: { type: Number, default: 0 }
 }, {
     timestamps: true
 });
+
+BlogPostViewsSchema.index({ blogID: 1 });
 
 export default mongoose.models.BlogPostViews || mongoose.model("BlogPostViews", BlogPostViewsSchema);
